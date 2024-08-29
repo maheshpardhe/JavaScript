091fc46da6a66c511ff55eac97c863445d31ba05
maheshpardhe <106740393+maheshpardhe@users.noreply.github.com>
2024-08-29 04:44:11 +0000

    conversion operation

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "18"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber);
+
+// "33" => 33
+// "331bc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "mahesh" =>true
+
+let someNumver =33
+
+let stringNumber = String(someNumver);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
